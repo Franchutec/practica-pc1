34cb3fc9ee113175b9da82f249749899548829ac
Franchutec <francolinares06@gmail.com>
2026-09-19 23:22:41 -0500

    feat(environment): add environment development and production variables configuration

---
+++ src/vite-env.d.ts
@@ -0,0 +1,13 @@
+﻿interface ImportMetaEnv {
+    VITE_NEWS_API_KEY: string;
+    VITE_NEWS_API_URL: string;
+    VITE_LOGO_API_URL: string;
+    VITE_LOGO_PUBLISHABLE_API_KEY: string;
+    VITE_SOURCES_ENDPOINT_PATH: string;
+    VITE_TOP_HEADLINES_ENDPOINT_PATH: string;
+    VITE_PRIME_UI_LICENSE_KEY: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
